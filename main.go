6ab67ba19e9955c6cb699beb9a07bab2c3ef9a1f
Brian Fajardo <brianfajardo15@gmail.com>
2021-11-28 00:15:11 -0500

    add routing to landing and contact page

--- main.go
+++ main.go
@@ -5,12 +5,30 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World!</h1>")
+func landingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Landing Page</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1>")
+}
+
+func routesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		landingHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found.", 404)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", routesHandler)
+
 	fmt.Println("Starting server on port :3000")
 	http.ListenAndServe(":3000", nil)
 }
