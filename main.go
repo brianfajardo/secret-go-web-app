6df944707e2c1e12d108341cf898fd42e9d193b4
Brian Fajardo <brianfajardo15@gmail.com>
2021-11-28 22:33:45 -0500

    install gorilla mux and use it

--- main.go
+++ main.go
@@ -3,38 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func landingHandler(w http.ResponseWriter, r *http.Request) {
+func landingPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Landing Page</h1>")
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func contactPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		landingHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		var errCode = http.StatusNotFound
-		var errMsg = http.StatusText(errCode)
-		http.Error(w, errMsg, errCode)
-	}
-}
-
 func main() {
-	var router Router
+	r := mux.NewRouter()
+	r.HandleFunc("/", landingPage)
+	r.HandleFunc("/contact", contactPage)
 
 	fmt.Println("Starting server on port :3000")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
-
-// WIP - pick up on lesson 3.4!
